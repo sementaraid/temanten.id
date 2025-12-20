579e4bb5b7451aa3133e81b37791aec9ee99c400
Nida <nida@192.168.1.4>
2025-12-20 16:56:22 +0700

    refresh package

--- src/main.tsx
+++ src/main.tsx
@@ -20,6 +20,6 @@ let router = createBrowserRouter([
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <RouterProvider router={router} />,
+    <RouterProvider router={router} />
   </StrictMode>
 )
