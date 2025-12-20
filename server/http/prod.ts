ae7c51b41ecf17997127393e09b31004d324b621
Nida <nida@192.168.1.4>
2025-12-20 20:04:42 +0700

    fix 404

--- server/http/prod.ts
+++ server/http/prod.ts
@@ -6,4 +6,7 @@ export const setupProdServer = async (app: Express) => {
   const distPath = path.resolve(root, 'dist')
 
   app.use(express.static(distPath))
+  app.use('/{*any}', (_req, res) => {
+    res.sendFile(path.join(distPath, 'index.html'))
+  })
 }
